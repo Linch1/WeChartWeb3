4d3142cfd08965bb70abb69ca75e4c422c668adc
Linch1 <m.carassiti@hotmail.it>
2022-06-25 01:49:07 +0200

    disabled whitelist in config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const { toCheckSum } = require("./utils/addresses");
 
 let scraperConfig = {
-    "whitelist_enabled": true,
+    "whitelist_enabled": false,
     "whitelist": [
         toCheckSum("0xc748673057861a797275CD8A068AbB95A902e8de")
     ] // whitelisted tokens
